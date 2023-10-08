f385339f4a56b485a1c98813178cf83fce822aa5
au410621106033 <147256797+au410621106033@users.noreply.github.com>
2023-10-08 07:07:55 -0800

    Add files via upload

---
+++ program 6.js
@@ -0,0 +1,10 @@
+var i = 3, mesg=" "
+ switch(i)
+  {
+     case 1: mesg = "One"
+      break; 
+      case 2: mesg = "Two"
+       break;
+        default: mesg = "Default";
+     }
+      document.write(mesg) 
